242883e98b09145d91390e959652ef2e0a8b3b15
David Wong <davidwongtt@gmail.com>
2021-01-23 19:16:00 +0000

    use Web3React

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-import { Paper, Grid } from '@material-ui/core';
+import { Web3ReactProvider } from '@web3-react/core'
+import { Grid } from '@material-ui/core';
+import Portis from '@portis/web3';
 import SearchAppBar from './components/SearchAppBar';
 import MainPlayer from './components/MainPlayer';
 import ProgressDataGrid from './components/ProgressDataGrid';
 import ProgressDescription from './components/ProgressDescription';
+import StateChannelsContainer from './containers/StateChannelsContainer';
 
+import { config as configDotEnv }from 'dotenv';
+configDotEnv();
+const portis = new Portis(process.env.PORTIS_DAPP_ADDRESS, process.env.DAPP_NETWORK);
 
+function getLibrary(provider, connector) {
+    return new ethers.providers.Web3Provider(portis.provider);
+  //return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+}
+
+import store from "./redux/store";
 
 class App extends Component {
   componentDidMount() {
   }
+
   render() {
     return (
       <Provider store={store}>
+      <Web3ReactProvider getLibrary={getLibrary}>
           <SearchAppBar/>
           <hr/>
           <Grid container alignContent="center" spacing={2}>
@@ -26,9 +39,10 @@ class App extends Component {
                 <ProgressDescription/>
             </Grid>
             <Grid item md={12} sm={12}>
-              <div width="100%">State Channels</div>
+              <StateChannelsContainer />
             </Grid>
           </Grid>
+      </Web3ReactProvider>
       </Provider>
     );
   }
---
+++ webclient/src/components/StateChannelsPannel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default class StateChannelsPanel extends React.Component {
+  render() {
+    return (
+      <div>statechannel</div>
+    );
+  }
+}
+
