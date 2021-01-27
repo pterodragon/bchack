ba700680d4728ef730aec5ec9f22ca65dfbf74f4
David Wong <davidwongtt@gmail.com>
2021-01-27 14:59:44 +0000

    better extention export

--- webtorrentx/lib/sidetalk.ts
+++ webtorrentx/lib/sidetalk.ts
@@ -5,6 +5,13 @@ import debug from 'debug';
 const log = debug('wx.sidetalk');
 const EXTENSION_NAME = "ut_sidetalk";
 
+const utSidetalk = ()=> {
+  //Extension class requires a "name" property on the prototype
+  SidetalkExtension.prototype.name = EXTENSION_NAME;
+  return SidetalkExtension;
+}
+export default utSidetalk;
+
 export interface SidetalkExtension {
   on(event: 'handshake', listener: (peerExtendedHandshake:object)=>any): this;
   on(event: 'message', listener: (msg: object)=>any): this;
@@ -28,7 +35,7 @@ export class SidetalkExtension extends EventEmitter implements Extension {
     //log('torrent bitfield', [torrent.bitfield]);
     log('use sidetalk');
     //wire.setTimeout(24 * 60 * 60);
-    wire.use(SidetalkExtension);
+    wire.use(utSidetalk());
     //workaround: see node_modules/bittorrent-protocol/index.js:373
     wire.peerExtendedMapping[EXTENSION_NAME] = wire._nextExt-1;
     return  wire[EXTENSION_NAME];
@@ -45,6 +52,4 @@ export class SidetalkExtension extends EventEmitter implements Extension {
     this.emit('message', msg);
   }
 }
-//Extension class requires a "name" property on the prototype
-SidetalkExtension.prototype.name = EXTENSION_NAME;
 
