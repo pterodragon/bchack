6557fa45ea9f8d8c997d2e282db16a8c0a840474
David Wong <davidwongtt@gmail.com>
2021-01-27 19:35:18 +0000

    export payment-ui

---
+++ payment-ui/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.components = require("./src/components/StateChannelsPannel");
+exports.actions = require("./src/redux/actions/statechanenls");
+exports.reducers = require("./src/redux/reducers/statechannels");
