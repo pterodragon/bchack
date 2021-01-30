550b69074f8d38507f6f644aee7edeb744ba0915
David Wong <davidwongtt@gmail.com>
2021-01-30 12:47:24 +0000

    wip

--- client-ui/lib/leecher.ts
+++ client-ui/lib/leecher.ts
@@ -1,11 +1,12 @@
 import {Instance as IWebTorrent, Torrent} from 'webtorrent';
+import { Instance as SimplePeer } from 'simple-peer';
 import {Wire} from "bittorrent-protocol";
 import {ethers, BigNumber, utils} from 'ethers';
-import createDebug from 'debug';
-import {WireSidetalk}  from './webtorrentx';
+import {WireSidetalk, WireUndestroyable}  from './webtorrentx';
 //assume client side handle the dotenv import
 import {StateChannelsPayment, PaymentInterface, Wallet} from './payment-statechannel';
 
+import createDebug from 'debug';
 const log = createDebug('wxp.leecher');
 
 const PIECE_PRICE = BigNumber.from(process.env.PIECE_PRICE);
@@ -20,9 +21,9 @@ export class Leecher {
     const payment = this.payment = new StateChannelsPayment(wallet);
 
     payment.on("handshakeBack", async(from: string, handshakeId: string, channelId: string, wire: Wire) => {
-      const depositAmount = PIECE_PRICE.mul(wire.length||1048576 + NUM_ALLOWANCE);
+      const depositAmount = PIECE_PRICE.mul(wire._length||1048576 + NUM_ALLOWANCE);
       const payload = await payment.deposit(from, depositAmount);
-      delete wire.length;
+      delete wire._length;
       await wire.ut_sidetalk.send({payload});
     });
     payment.on("requested", async(from, amount, agree, wire)=> {
@@ -38,9 +39,7 @@ export class Leecher {
     const {payment} = this;
 
     const torrent = this.client.add(uri);
-    let destroy = torrent.destroy;
-    console.log({destroy});
-    torrent.destroy = ()=>console.log('torrent cannot destroy now');
+
     torrent.on('download', (bytes: number) => log('download', bytes));
     torrent.on('upload', (bytes: number) => log('upload', bytes));
 
@@ -48,8 +47,10 @@ export class Leecher {
       log('wire', wire.nodeId, wire.peerId);
       if (wire.peerId !== '2d5757303031322d724a32683939617936376c5c') return;
       wire.setKeepAlive(true);
-      wire.length = torrent.length;
-      console.log('wire.length', wire.length);
+      wire._length = torrent.length;
+      log('wire._length', wire._length);
+
+      const undestroy = WireUndestroyable.extend(wire);
 
       const sidetalk = WireSidetalk.extend(wire);
       sidetalk.on('handshake', async(handshake)=> {
@@ -68,15 +69,12 @@ export class Leecher {
         const payload = await payment.finalize(wire.address);
         await sidetalk.send({payload});
         delete wire.address;
+        undestroy.release();
       });
 
     });
 
-    torrent.on('done', ()=> {
-      torrent.destroy = destroy.bind(torrent);
-      destroy = undefined;
-    });
-
-    return torrent; }
+    return torrent; 
+  }
 
 }
